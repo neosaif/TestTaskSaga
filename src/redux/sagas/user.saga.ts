1aa1605d816fa0e9f6988539afed24fb62b086d0
neosaif <saifali@neoito.com>
2022-07-28 09:37:28 +0530

    redux-changes

--- src/redux/sagas/user.saga.ts
+++ src/redux/sagas/user.saga.ts
@@ -4,13 +4,13 @@ import { createUserSlice, deleteUserSlice, updateUserSlice, getUsersSlice } from
 import { setCount } from '../slice/count.slice'
 import { CREATE_USER, DELETE_USER, GET_USERS_LIST, GET_USER_DETAILS, UPDATE_USER } from '../actions'
 import { put, takeEvery } from 'redux-saga/effects'
-export function* getUsersSaga(action) {    
+export function* getUsersListSaga(action) {    
     const users = yield getUsersList(action.query)
     yield put(setCount(users.headers["x-total-count"]))
     yield put(getUsersSlice(users.data))
 }
 
-export function* getUserByIdSaga(action) {
+export function* getUserDetailsSaga(action) {
     yield getUserDetails(action.id)
     yield put(setUserSlice(action.id))
 }
@@ -25,15 +25,15 @@ export function* updateUserSaga(action) {
     yield put(updateUserSlice(action.user))
 }
 
-export function* deleteUserByIdSaga(action) {
+export function* deleteUserSaga(action) {
     yield deleteUser(action.id)
     yield put(deleteUserSlice(action.id))
 }
 
 export function* watchUsersAsync() {
-    yield takeEvery(GET_USERS_LIST, getUsersSaga)
-    yield takeEvery(GET_USER_DETAILS, getUserByIdSaga)
+    yield takeEvery(GET_USERS_LIST, getUsersListSaga)
+    yield takeEvery(GET_USER_DETAILS, getUserDetailsSaga)
     yield takeEvery(CREATE_USER, createUserSaga)
     yield takeEvery(UPDATE_USER, updateUserSaga)
-    yield takeEvery(DELETE_USER, deleteUserByIdSaga)
+    yield takeEvery(DELETE_USER, deleteUserSaga)
 }
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,13 +1,16 @@
 
+// import { createStore, applyMiddleware } from 'redux';
 import { configureStore } from "@reduxjs/toolkit";
-import createSagaMiddleware from "@redux-saga/core";
+import createSagaMiddleware from 'redux-saga';
 import {rootSaga} from './root.saga';
 import {rootReducer} from './root.reducer';
 const sagaMiddleware = createSagaMiddleware()
+// const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 const store = configureStore({
     reducer:rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false, serializableCheck: false }).concat(sagaMiddleware)
 })
 sagaMiddleware.run(rootSaga)
 
 export default store
+
